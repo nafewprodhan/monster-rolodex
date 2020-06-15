9d15b8d0394a4ad7bd68afedd59a8e8a892e548e
Nafew Prodhan <nafew.prodhan@gmail.com>
2020-06-15 20:57:45 +0600

    Updates

---
+++ precache-manifest.72d2868bfc226f1f6d164272de9920dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4c8b56e929b10f9b9d668a77318b4b5",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "e911a3baf2755d42525c",
+    "url": "/monster-rolodex/static/css/main.84d32335.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e911a3baf2755d42525c",
+    "url": "/monster-rolodex/static/js/main.ad56a1ce.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
